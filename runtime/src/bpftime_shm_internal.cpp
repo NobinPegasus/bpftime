f9354aabc4f8af9a3d9372a9028a4932371c86e7
NobinPegasus <zeshanahmednobin@gmail.com>
2024-04-21 14:33:34 +0600

    Allow defining shared memory size at runtime #282

--- runtime/src/bpftime_shm_internal.cpp
+++ runtime/src/bpftime_shm_internal.cpp
@@ -19,6 +19,8 @@
 
 static bool global_shm_initialized = false;
 
+size_t BPFTIME_SHARED_MEMORY_SIZE = 20; // Default size
+
 extern "C" void bpftime_initialize_global_shm(bpftime::shm_open_type type)
 {
 	using namespace bpftime;
@@ -568,7 +570,7 @@ bpftime_shm::bpftime_shm(const char *shm_name, shm_open_type type)
 		segment = boost::interprocess::managed_shared_memory(
 			boost::interprocess::open_or_create,
 			// Allocate 20M bytes of memory by default
-			shm_name, 20 << 20);
+			shm_name, BPFTIME_SHARED_MEMORY_SIZE << 20);
 
 		manager = segment.find_or_construct<bpftime::handler_manager>(
 			bpftime::DEFAULT_GLOBAL_HANDLER_NAME)(segment);
@@ -601,7 +603,7 @@ bpftime_shm::bpftime_shm(const char *shm_name, shm_open_type type)
 		segment = boost::interprocess::managed_shared_memory(
 			boost::interprocess::create_only,
 			// Allocate 20M bytes of memory by default
-			shm_name, 20 << 20);
+			shm_name, BPFTIME_SHARED_MEMORY_SIZE << 20);
 		SPDLOG_DEBUG("done: bpftime_shm for server setup: segment");
 
 		manager = segment.construct<bpftime::handler_manager>(
